c710a6e33ecc6fea2c75fd726c248710010491a3
unknown <kjc38@ZUSE.csad.cs.calvin.edu>
2015-09-25 14:39:06 -0400

    Initial commit

---
+++ app/src/main/java/edu/calvin/cs262/newhomework1/MainActivity.java
@@ -0,0 +1,61 @@
+package edu.calvin.cs262.newhomework1;
+
+import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.app.ActionBar;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        //int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        //if (id == R.id.action_settings) {
+        //    return true;
+       // }
+
+        //return super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    // Had to add these definitions
+    private void openSearch(){
+        startActivity(new Intent(SearchManager.INTENT_ACTION_GLOBAL_SEARCH));
+    }
+
+    private void openSettings(){
+        startActivity(new Intent(Settings.ACTION_SETTINGS));
+    }
+
+}
